ccd56a9e34c56241adfe76d0d4536bee91dc641b
junchh <13518044@std.stei.itb.ac.id>
2022-02-23 20:42:43 +0700

    embed okay

--- main.js
+++ main.js
@@ -1,7 +1,168 @@
 const { pi } = require("mathjs");
 const math = require("mathjs");
+const inkjet = require("inkjet");
+const fs = require("fs");
 
-const initial = [5, 4, 3, -29, 32, 9, 1, -0.988];
+const parseMap = (data) => {
+  const result = [];
+  const features = data.features;
+  for (let i = 0; i < features.length; i++) {
+    const geometry = features[i].geometry;
+
+    if (geometry.type === "Point") {
+      result.push(
+        math.complex(geometry.coordinates[0], geometry.coordinates[1])
+      );
+    } else if (geometry.type === "LineString") {
+      for (let j = 0; j < geometry.coordinates.length; j++) {
+        result.push(
+          math.complex(geometry.coordinates[j][0], geometry.coordinates[j][1])
+        );
+      }
+    } else if (geometry.type === "Polygon") {
+      for (let j = 0; j < geometry.coordinates.length; j++) {
+        for (let k = 0; k < geometry.coordinates[j].length; k++) {
+          result.push(
+            math.complex(
+              geometry.coordinates[j][k][0],
+              geometry.coordinates[j][k][1]
+            )
+          );
+        }
+      }
+    } else if (geometry.type === "MultiPoint") {
+      for (let j = 0; j < geometry.coordinates.length; j++) {
+        result.push(
+          math.complex(geometry.coordinates[j][0], geometry.coordinates[j][1])
+        );
+      }
+    } else if (geometry.type === "MultiLineString") {
+      for (let j = 0; j < geometry.coordinates.length; j++) {
+        for (let k = 0; k < geometry.coordinates[j].length; k++) {
+          result.push(
+            math.complex(
+              geometry.coordinates[j][k][0],
+              geometry.coordinates[j][k][1]
+            )
+          );
+        }
+      }
+    } else if (geometry.type === "MultiPolygon") {
+      for (let j = 0; j < geometry.coordinates.length; j++) {
+        for (let k = 0; k < geometry.coordinates[j].length; k++) {
+          for (let l = 0; l < geometry.coordinates[j][k].length; l++) {
+            result.push(
+              math.complex(
+                geometry.coordinates[j][k][l][0],
+                geometry.coordinates[j][k][l][1]
+              )
+            );
+          }
+        }
+      }
+    }
+  }
+  return result;
+};
+
+const parseComplex = (data, originalMap) => {
+  const features = originalMap.features;
+  let ptr = 0;
+  for (let i = 0; i < features.length; i++) {
+    const geometry = features[i].geometry;
+
+    if (geometry.type === "Point") {
+      geometry.coordinates[0] = data[ptr].re;
+      geometry.coordinates[1] = data[ptr].im;
+      ptr++;
+    } else if (geometry.type === "LineString") {
+      for (let j = 0; j < geometry.coordinates.length; j++) {
+        geometry.coordinates[j][0] = data[ptr].re;
+        geometry.coordinates[j][1] = data[ptr].im;
+        ptr++;
+      }
+    } else if (geometry.type === "Polygon") {
+      for (let j = 0; j < geometry.coordinates.length; j++) {
+        for (let k = 0; k < geometry.coordinates[j].length; k++) {
+          geometry.coordinates[j][k][0] = data[ptr].re;
+          geometry.coordinates[j][k][1] = data[ptr].im;
+          ptr++;
+        }
+      }
+    } else if (geometry.type === "MultiPoint") {
+      for (let j = 0; j < geometry.coordinates.length; j++) {
+        geometry.coordinates[j][0] = data[ptr].re;
+        geometry.coordinates[j][1] = data[ptr].im;
+        ptr++;
+      }
+    } else if (geometry.type === "MultiLineString") {
+      for (let j = 0; j < geometry.coordinates.length; j++) {
+        for (let k = 0; k < geometry.coordinates[j].length; k++) {
+          geometry.coordinates[j][k][0] = data[ptr].re;
+          geometry.coordinates[j][k][1] = data[ptr].im;
+          ptr++;
+        }
+      }
+    } else if (geometry.type === "MultiPolygon") {
+      for (let j = 0; j < geometry.coordinates.length; j++) {
+        for (let k = 0; k < geometry.coordinates[j].length; k++) {
+          for (let l = 0; l < geometry.coordinates[j][k].length; l++) {
+            geometry.coordinates[j][k][l][0] = data[ptr].re;
+            geometry.coordinates[j][k][l][1] = data[ptr].im;
+            ptr++;
+          }
+        }
+      }
+    }
+  }
+};
+
+const convToBlackWhite = (arr) => {
+  const res = [];
+  let i = 0;
+  for (let i = 0; i < arr.length; i += 4) {
+    const cur =
+      (arr[i] * 0.2126 + arr[i + 1] * 0.7152 + arr[i + 2] * 0.0722) / 255;
+    if (cur >= 0.5) {
+      res.push(0);
+    } else {
+      res.push(1);
+    }
+  }
+  return res;
+};
+
+const saveToImage = (filename, arr, width, height) => {
+  const frameData = Buffer.alloc(width * height * 4);
+  let i = 0;
+  let k = 0;
+
+  while (i < frameData.length) {
+    if (arr[k] === 1) {
+      frameData[i++] = 0;
+      frameData[i++] = 0;
+      frameData[i++] = 0;
+      frameData[i++] = 0xff;
+    } else {
+      frameData[i++] = 255;
+      frameData[i++] = 255;
+      frameData[i++] = 255;
+      frameData[i++] = 0xff;
+    }
+    k++;
+  }
+
+  const buf = frameData;
+  const options = {
+    width: width,
+    height: height,
+    quality: 80,
+  };
+
+  inkjet.encode(buf, options, (err, encoded) => {
+    fs.writeFileSync(filename, encoded.data);
+  });
+};
 
 const dft_normal = (arr) => {
   const n = arr.length;
@@ -70,9 +231,81 @@ const fft = (a, invert) => {
     }
   }
 };
-console.log(dft_normal(initial));
-console.log(initial);
-fft(initial, 1);
-console.log(initial);
-fft(initial, -1);
-console.log(initial);
+
+const normalize = (phase) => {
+  let phaseValue = phase % (2 * math.pi);
+  if (phaseValue < 0) {
+    phaseValue = (phaseValue + 2 * math.pi) % (2 * math.pi);
+  }
+  return phaseValue;
+};
+
+const getCategory = (phase, step) => {
+  const angle = (2 * math.pi) / step;
+
+  const region = Math.floor(phase / angle);
+  if (region % 2 === 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+const embed = (map, watermark, step) => {
+  const stepNormalized = (step / 360.0) * 2 * math.pi;
+  const mapData = map;
+  let cur = 1;
+  while (mapData.length > cur) {
+    cur *= 2;
+  }
+  let diff = cur - mapData.length;
+  for (let i = 0; i < diff; i++) {
+    mapData.push(math.complex(0));
+  }
+  fft(mapData, 1);
+
+  const copyof = [...mapData];
+  for (let i = 0; i < watermark.length; i++) {
+    const polarForm = mapData[i].toPolar();
+    const phi = normalize(polarForm.phi);
+    if (getCategory(phi, stepNormalized) !== watermark[i]) {
+      const newAngle = (phi + stepNormalized) % (2 * math.pi);
+      const r = polarForm.r;
+      mapData[i] = math.complex({ r: r, phi: newAngle });
+    }
+  }
+  fft(mapData, -1);
+};
+// console.log(dft_normal(initial));
+// console.log(initial);
+// fft(initial, 1);
+// console.log(initial);
+// fft(initial, -1);
+// console.log(initial);
+
+const cmd = process.argv[2];
+if (cmd === "embed") {
+  const mapfilename = process.argv[3];
+  const watermarkfilename = process.argv[4];
+  const targetname = process.argv[5];
+
+  let rawdata = fs.readFileSync(mapfilename);
+  let data = JSON.parse(rawdata);
+
+  const mapData = parseMap(data);
+
+  const watermarkBuf = fs.readFileSync(watermarkfilename);
+
+  inkjet.decode(watermarkBuf, (err, decoded) => {
+    if (err) {
+      console.log(err);
+    }
+    const watermarkArray = convToBlackWhite(decoded.data);
+
+    embed(mapData, watermarkArray, 0.02);
+    parseComplex(mapData, data);
+
+    fs.writeFileSync(targetname, JSON.stringify(data));
+  });
+} else if (cmd === "extract") {
+}
